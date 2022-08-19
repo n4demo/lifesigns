e5368337f6ace3ebf412570432fa2d7739f330b5
n.wardle@node4.co.uke <n.wardle@node4.co.uk>
2022-08-19 15:23:45 +0100

    .

--- LifeSigns/Processor.cs
+++ LifeSigns/Processor.cs
@@ -19,34 +19,34 @@ namespace LifeSigns
         {
             var readings = new Readings();
             var random = new Random();
-            int randomInt = random.Next(0,50) - 25;
+            int randomInt = random.Next(0,40) - 20;
             double randomDouble = random.NextDouble() - 0.5;
 
-            int second = DateTime.Now.Second;
+            int minute = DateTime.Now.Minute;
 
-            if (second < 16)
+            if (minute < 16)
             { }
-            else if (second < 41)
+            else if (minute < 41)
             {
                 readings.DiaStolic += 20;
                 readings.Systolic += 25;
                 readings.HeartRate += 35;
-                readings.SpO2 += -11;
+                readings.SpO2 += -5;
                 readings.Temperature += 1.8;
             }
-            else if (second < 59)
+            else if (minute < 59)
             {
                 readings.DiaStolic += 35;
                 readings.Systolic += 57;
                 readings.HeartRate += 75;
-                readings.SpO2 += -18;
+                readings.SpO2 += -11;
                 readings.Temperature += 3.5;
             }
 
             readings.DiaStolic += randomInt;
             readings.Systolic += randomInt;
             readings.HeartRate += randomInt;
-            readings.SpO2 += randomDouble;
+            readings.SpO2 += randomInt;
             readings.Temperature += randomDouble;
 
             if (readings.SpO2 > 99) readings.SpO2 = 99;
